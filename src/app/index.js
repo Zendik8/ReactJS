e930e14fdee31a8410970451f974d3832f7662e5
Nazarii Bodnarchuk <zendik8@ukr.net>
2016-12-26 18:38:27 +0000

    will be min change

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,31 @@
-console.log("It works");
+import React from "react";
+import { render } from "react-dom";
+
+import{ Header } from "./components/Header";
+import{ Home } from "./components/Home";
+
+class App extends React.Component {
+    render(){
+        var user = {
+            name: "Victoriia",
+            hobbies: ["Makeup", "Music"]
+        };
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Header/>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-xs-10 col-xs-offset-1">
+                        <Home name = {"Nazarii"} age = { 25 } user = { user }>
+                            <p>paragraph</p>
+                        </Home>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+render(<App/>, document.getElementById("app"));
